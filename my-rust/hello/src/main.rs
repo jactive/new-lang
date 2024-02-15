d46432f95a71d2d60e6bf5f08232ec838326c60c
Haifeng Won <haifengwon@hotmail.com>
2024-02-14 20:29:46 -0800

    before first web page

--- my-rust/hello/src/main.rs
+++ my-rust/hello/src/main.rs
@@ -1,5 +1,48 @@
+// import FromStr trait. A trait is a collection of methods like j.interface
+// FromStr trait defines an abstract method, u64::from_str
+// FromStr trait must be imported, although it is never used elsewhere
+use std::str::FromStr;
+// import std::env module. env::args() is a function to access cmd args
+use std::env;
+
 fn main() {
-    println!("{}", gcd(111, 221));
+    // mut tells Rush to allow calling numbers.push
+    // numbers declaration would better to be
+    //    let mut numbers: Vec<u64> = Vec::new();
+    // however, Rust is not picky. Rust can infer Vec<u64>.
+    // Rust will complain about different type of values pushed into numbers
+    let mut numbers = Vec::new();
+    
+    // env is imported by `use std::env`
+    for arg in env::args().skip(1) {
+        // u64::from_str is static method of u64 type
+        // imported/weaved_in by `use std::str::FromStr`.
+        //
+        // u64::from_str doesn’t return a u64 directly,
+        // but rather a *Result* value that indicates
+        // whether the parse succeeded or failed.
+        // Result is an enum, either Ok(v) or Err(e)
+        // OK(v).expect(&str) returns the parsed u64 value
+        numbers.push(u64::from_str(&arg)
+                      .expect("error parsing argument"));
+    }
+
+    if numbers.len() == 0 {
+        eprintln!("Usage: gcd Number ...");
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    // &numbers[1..] lets m borrow the reference of the
+    // elements in numbers
+    // *m dereferences the reference, yielding the value
+    // referenced by m
+    for m in &numbers[1..] {
+         d = gcd(d, *m);
+    }
+
+    // println! is a macro that takes a template string
+    println!("The greatest common divisor {:?} is {}", numbers, d);
 }
 
 fn gcd(mut n: u64, mut m: u64) -> u64 {
@@ -25,6 +68,7 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
     n
 }
 
+// #[test] marker is an attribute (annotation in Java)
 #[test]
 fn test_gcd( ){
     assert_eq!(gcd(14, 15), 1);
